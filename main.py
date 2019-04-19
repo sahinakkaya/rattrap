a7a761a4e14188b268cdc743effd49193ada47c1
Asocia <akkayas17@itu.edu.tr>
2019-04-20 02:49:04 +0300

    Revert "Merge pull request #3 from Asocia/experimental"
    This reverts commit 7a19931a57060c286513954dcf966e4f2444fd41, reversing
    changes made to d15dc8962ee7dbf67a91f4835a62499b12965a19.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from src.rattrap import RattrapWindow
-from src.ratslap import PermissionDeniedError
 
 # TODO: Add functionality to manually set shortcuts
 # TODO: Make color selection more visual with actual colors
@@ -11,11 +10,5 @@ from src.ratslap import PermissionDeniedError
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    try:
-        main_window = RattrapWindow()
-        sys.exit(app.exec_())
-    except PermissionDeniedError:
-        pass
-    except Exception as e:
-        print(e)
-        exit(-1)
+    main_window = RattrapWindow()
+    sys.exit(app.exec_())
--- shell_scripts/find_keymap.sh
+++ shell_scripts/find_keymap.sh
@@ -3,4 +3,4 @@
 keycode=$1
 keycode=$(printf "%3s" "${keycode}")
 
-xmodmap -pke | grep -e "keycode ${keycode}" | sed 's/^.*= //'
+xmodmap -pke | grep -e "keycode ${keycode}" | cut -d' ' -f "5 6"
